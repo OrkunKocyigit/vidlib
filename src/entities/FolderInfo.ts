119828336255e437cbfc42998c692300bb0307dd
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-07-12 09:42:25 +0300

    Added folder info icon

--- src/entities/FolderInfo.ts
+++ src/entities/FolderInfo.ts
@@ -3,5 +3,6 @@ import {VideoFile} from "./VideoFile";
 export interface FolderInfo {
     path: string,
     folders: FolderInfo[],
-    videos: VideoFile[]
+    videos: VideoFile[],
+    empty: boolean
 }
--- src/pages/main/components/FolderInfoView.tsx
+++ src/pages/main/components/FolderInfoView.tsx
@@ -2,15 +2,37 @@ import {FolderInfo} from "../../../entities/FolderInfo";
 import React from "react";
 import FileTreeView from "./FileTreeView";
 import VideoFileView from "./VideoFileView";
+import {ArrowDown, ArrowRight} from "tabler-icons-react";
+import {Group, Text, ThemeIcon, UnstyledButton} from "@mantine/core";
 
 type Props = {
     folder: FolderInfo
 }
 
+function renderFolderIcon(folder: FolderInfo, state: boolean) {
+    if (folder.empty) {
+        return null
+    }
+    return (
+        <ThemeIcon>
+            {state ?
+                <ArrowDown></ArrowDown>
+                :
+                <ArrowRight></ArrowRight>
+            }
+        </ThemeIcon>
+    )
+}
+
 function FolderInfoView(props: Props) {
     return (
         <div>
-            {JSON.stringify(props.folder, null, 4)}
+            <UnstyledButton>
+                <Group>
+                    {renderFolderIcon(props.folder, false)}
+                    <Text>{props.folder.path}</Text>
+                </Group>
+            </UnstyledButton>
             <FileTreeView folders={props.folder.folders}></FileTreeView>
             {props.folder.videos.map(video => <VideoFileView video={video} key={video.path}/>)}
         </div>
