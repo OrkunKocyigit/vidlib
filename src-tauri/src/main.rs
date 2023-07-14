85d4eb1c0eb261f366880d741053ab9a57c36135
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-07-14 09:30:44 +0300

    Add sqlite connection into state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use crate::database::load_database;
+use rusqlite::Connection;
 use std::path::PathBuf;
+use std::sync::Mutex;
+use tauri::Manager;
 
 use crate::filescan::FolderInfo;
 use crate::service::Response;
@@ -10,6 +14,10 @@ mod database;
 mod filescan;
 mod gui;
 mod service;
+
+struct AppState {
+    pub db: Mutex<Option<Connection>>,
+}
 
 #[tauri::command]
 async fn file_scan(path: String) -> Result<Response<FolderInfo>, ()> {
@@ -23,7 +31,17 @@ fn select_folder() -> Result<Response<PathBuf>, ()> {
 
 fn main() {
     tauri::Builder::default()
+        .manage(AppState {
+            db: Default::default(),
+        })
         .invoke_handler(tauri::generate_handler![file_scan, select_folder])
+        .setup(|app| {
+            let handle = app.handle();
+            let state = handle.state::<AppState>();
+            let db = load_database(&handle).expect("Load database failed");
+            *state.db.lock().unwrap() = Some(db);
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
