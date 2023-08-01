b764cc061b9ff098a6b257d0aa34533e605f8e47
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-08-01 18:19:02 +0300

    fix: Video name not updating on screen

--- src/pages/video/components/VideoName.tsx
+++ src/pages/video/components/VideoName.tsx
@@ -1,25 +1,31 @@
 import { type VideoFile } from '../../../entities/VideoFile';
 import UpdatableText from '../../../components/UpdatableText';
 import { SetName } from '../../../service/SetName';
+import { useEffect, useState } from 'react';
 
 export interface VideoNameProps {
   video: VideoFile;
 }
 function VideoName(props: VideoNameProps): JSX.Element {
-  function setText(name: string): void {
-    SetName(props.video, name)
+  const [name, setName] = useState(props.video.video?.name);
+  function setText(newName: string): void {
+    SetName(props.video, newName)
       .then((value) => {
-        const { response: name = '' } = value;
-        props.video.name = name;
+        const { response: responseName = '' } = value;
+        setName(responseName);
       })
       .catch((reason) => {
         console.error(reason);
       });
   }
 
+  useEffect(() => {
+    setName(props.video.video?.name);
+  }, [props.video]);
+
   return (
     <UpdatableText
-      text={props.video.name}
+      text={name ?? ''}
       setText={setText}
       titleProps={{ order: 4 }}
       editButtonProps={{ color: 'blue' }}
