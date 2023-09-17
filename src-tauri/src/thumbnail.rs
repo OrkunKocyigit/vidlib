86449e9aa4a263e2a6842991ce74a6efd3f905ff
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-09-17 16:13:39 +0300

    refactor: Implemented ffmpeg thumbnail creator

--- src-tauri/src/thumbnail.rs
+++ src-tauri/src/thumbnail.rs
@@ -1,6 +1,20 @@
+use std::ffi::CString;
 use std::fmt::Formatter;
-use std::{collections, fmt, fs, path};
+use std::fs::File;
+use std::io::Write;
+use std::{collections, fmt, fs, path, slice};
 
+use anyhow::{bail, Context};
+use rsmpeg::avcodec::{AVCodec, AVCodecContext};
+use rsmpeg::avformat::AVFormatContextInput;
+use rsmpeg::avutil::{AVFrameWithImage, AVImage};
+use rsmpeg::error::RsmpegError;
+use rsmpeg::ffi;
+use rsmpeg::ffi::{
+    av_rescale, av_seek_frame, AVCodecID_AV_CODEC_ID_PNG, AVSEEK_FLAG_BACKWARD, AVSEEK_FLAG_FRAME,
+    AV_TIME_BASE,
+};
+use rsmpeg::swscale::SwsContext;
 use serde::Serialize;
 use tauri::{AppHandle, Manager};
 use tokio::sync;
@@ -188,22 +202,135 @@ fn create_thumbnail(
     id: &String,
     video_location: &path::PathBuf,
 ) -> path::PathBuf {
-    let file_name = &format!("{}_01.png", id);
-    let full_location = &save_location.join(file_name);
-    let path = generate_thumbnail(file_name, full_location, video_location);
-    match path {
-        Ok(p) => p,
-        Err(e) => {
-            error!("Failed to generate thumbnail: {}", e);
-            path::PathBuf::from("./images/image_not_found.webp")
-        }
-    }
+    let file_name = format!("{}_01.png", id);
+    let full_location = save_location.join(file_name);
+    generate_thumbnail(&full_location, &video_location).unwrap_or_else(|e| {
+        error!("Failed to generate thumbnail: {}", e);
+        path::PathBuf::from("./images/image_not_found.webp")
+    })
 }
 
 fn generate_thumbnail(
-    save_location: &String,
-    file_name: &path::PathBuf,
+    save_location: &path::PathBuf,
     video_location: &path::PathBuf,
 ) -> Result<path::PathBuf, anyhow::Error> {
-    Err(anyhow::Error::msg("Test"))
+    debug!("Generate thumbnail started {}", video_location.display());
+    let mut input_context = AVFormatContextInput::open(
+        &CString::new(video_location.to_string_lossy().as_bytes())
+            .context("Video location can't be converted")?,
+    )
+    .context("Video file input context failed")?;
+    debug!("Input context created");
+    let (video_index, video_codec) = input_context
+        .find_best_stream(ffi::AVMediaType_AVMEDIA_TYPE_VIDEO)
+        .context("Video stream can't be find")?
+        .context("Video stream codec can't be find")?;
+    let (mut decoder_context, seek_location) = {
+        let video_stream = input_context
+            .streams()
+            .get(video_index)
+            .context("Video stream with index can't be find")?;
+        let mut codec_context = AVCodecContext::new(&video_codec);
+        let codec_parameters = video_stream.codecpar();
+        codec_context
+            .apply_codecpar(&codec_parameters)
+            .context("Failed to apply codec parameters")?;
+        codec_context
+            .open(None)
+            .context("Can't open the codec context")?;
+        (codec_context, video_stream.duration / 2)
+    };
+    debug!("Decoding context created");
+    unsafe {
+        av_seek_frame(
+            input_context.as_mut_ptr(),
+            video_index as i32,
+            seek_location,
+            (AVSEEK_FLAG_BACKWARD | AVSEEK_FLAG_FRAME) as i32,
+        )
+    };
+    debug!("Seeked to middle of the video");
+    let thumbnail_frame = loop {
+        let thumbnail_packet = loop {
+            match input_context.read_packet()? {
+                Some(x) if x.stream_index != video_index as i32 => {}
+                x => break x,
+            }
+        };
+        debug!("Sending packet to decoder");
+        decoder_context.send_packet(thumbnail_packet.as_ref())?;
+        match decoder_context.receive_frame() {
+            Ok(x) => break x,
+            Err(RsmpegError::DecoderDrainError) => {}
+            Err(e) => bail!(e),
+        }
+        if thumbnail_packet.is_none() {
+            bail!("Can't find video cover frame");
+        }
+    };
+    debug!("Found thumbnail frame");
+    let mut encoder_context = {
+        let encoder_codec =
+            AVCodec::find_encoder(AVCodecID_AV_CODEC_ID_PNG).context("Can't find the encoder")?;
+        let mut encoder_context = AVCodecContext::new(&encoder_codec);
+        encoder_context.set_bit_rate(decoder_context.bit_rate);
+        let display_aspect_ratio = thumbnail_frame.width as f64 / thumbnail_frame.height as f64;
+        encoder_context.set_width((450f64 * display_aspect_ratio) as i32);
+        encoder_context.set_height(450);
+        encoder_context.set_time_base(ffi::av_inv_q(decoder_context.framerate));
+        encoder_context.set_pix_fmt(if let Some(pix_fmts) = encoder_codec.pix_fmts() {
+            pix_fmts[0]
+        } else {
+            decoder_context.pix_fmt
+        });
+        encoder_context
+            .open(None)
+            .context("Can't open encoder context")?;
+        encoder_context
+    };
+    debug!("Encoding context created");
+    let scaled_thumbnail_packet = {
+        let mut sws_context = SwsContext::get_context(
+            decoder_context.width,
+            decoder_context.height,
+            decoder_context.pix_fmt,
+            encoder_context.width,
+            encoder_context.height,
+            encoder_context.pix_fmt,
+            ffi::SWS_LANCZOS,
+        )
+        .context("Can't create software scaler context")?;
+        let image_buffer = AVImage::new(
+            encoder_context.pix_fmt,
+            encoder_context.width,
+            encoder_context.height,
+            1,
+        )
+        .context("Can't create image buffer")?;
+        let mut scaled_thumbnail_frame = AVFrameWithImage::new(image_buffer);
+        sws_context.scale_frame(
+            &thumbnail_frame,
+            0,
+            decoder_context.height,
+            &mut scaled_thumbnail_frame,
+        )?;
+        encoder_context.send_frame(Some(&scaled_thumbnail_frame))?;
+        encoder_context.receive_packet()?
+    };
+    debug!("Thumbnail image scaled");
+    let data = unsafe {
+        slice::from_raw_parts(
+            scaled_thumbnail_packet.data,
+            scaled_thumbnail_packet.size as usize,
+        )
+    };
+    return save_image(save_location, data);
+}
+
+fn save_image(save_location: &path::PathBuf, data: &[u8]) -> Result<path::PathBuf, anyhow::Error> {
+    debug!("Start saving image");
+    let mut file = File::create(save_location)?;
+    file.write_all(data)?;
+    info!("Thumbnail created successfully {}", save_location.display());
+    Ok(save_location.clone())
 }
