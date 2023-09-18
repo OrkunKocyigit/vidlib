426a8c5a7abd8ecb8a519269c7869258cce0a44d
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-09-18 08:35:07 +0300

    refactor: is_video validation moved to ffmpeg

--- src-tauri/src/video.rs
+++ src-tauri/src/video.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 
+use rsmpeg::ffi::AVMediaType_AVMEDIA_TYPE_VIDEO;
 use serde::{Deserialize, Serialize};
+
+use crate::thumbnail;
 
 #[derive(Deserialize, Serialize, Clone)]
 pub struct VideoCategory {
@@ -57,16 +59,40 @@ impl VideoEntry {
     }
 }
 
+pub const VIDEO_FILE_EXTENSIONS: &[&str] = &[
+    "3g2", "3gp", "3gp2", "3gpp", "amv", "asf", "avi", "bik", "dds", "divx", "dpg", "dv", "dvr-ms",
+    "evo", "f4v", "flv", "hdmov", "k3g", "m1v", "m2t", "m2ts", "m2v", "m4b", "m4p", "m4v", "mk3d",
+    "mkv", "mov", "mp2v", "mp4", "mp4v", "mpe", "mpeg", "mpg", "mpv2", "mpv4", "mqv", "mts", "mxf",
+    "nsv", "ogm", "ogv", "qt", "ram", "rm", "rmvb", "skm", "swf", "tp", "tpr", "trp", "ts", "vob",
+    "webm", "wm", "wmv", "xvid",
+];
+
 pub fn is_video<P: AsRef<Path>>(path: P) -> bool {
-    let mut file = match File::open(path) {
-        Ok(f) => f,
-        _ => return false,
-    };
-    let mut buf = vec![0; 1024];
-    let result = file.read_exact(&mut buf);
-    if result.is_err() {
-        false
+    let path = path.as_ref();
+    if is_file_openable(path) && has_video_extension(path) {
+        has_video_stream(path)
     } else {
-        infer::is_video(&*buf)
+        false
+    }
+}
+
+fn is_file_openable<P: AsRef<Path>>(path: P) -> bool {
+    File::open(path).is_ok()
+}
+
+fn has_video_extension<P: AsRef<Path>>(path: P) -> bool {
+    path.as_ref()
+        .extension()
+        .map(|e| VIDEO_FILE_EXTENSIONS.contains(&e.to_string_lossy().as_ref()))
+        .unwrap_or(false)
+}
+
+fn has_video_stream<P: AsRef<Path>>(path: P) -> bool {
+    match thumbnail::create_input_context(path) {
+        Ok(input_context) => input_context
+            .streams()
+            .into_iter()
+            .any(|s| s.codecpar().codec_type == AVMediaType_AVMEDIA_TYPE_VIDEO),
+        Err(_) => false,
     }
 }
