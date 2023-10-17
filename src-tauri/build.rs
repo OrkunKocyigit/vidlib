2a5baddcbca17e51b1f810f3ffab0ccf4daa9e81
Orkun Kocyigit <orkunkocyigit@gmx.com>
2023-10-17 14:21:24 +0300

    chore: Bump dependencies to support ffmpeg 6

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,16 @@
 fn main() {
-  tauri_build::build()
+    if cfg!(windows) {
+        println!("cargo:rustc-link-lib=dxva2");
+        println!("cargo:rustc-link-lib=evr");
+        println!("cargo:rustc-link-lib=mf");
+        println!("cargo:rustc-link-lib=mfplat");
+        println!("cargo:rustc-link-lib=mfplay");
+        println!("cargo:rustc-link-lib=mfreadwrite");
+        println!("cargo:rustc-link-lib=mfuuid");
+        println!("cargo:rustc-link-lib=bcrypt");
+        println!("cargo:rustc-link-lib=ws2_32");
+        println!("cargo:rustc-link-lib=Secur32");
+        println!("cargo:rustc-link-lib=Strmiids");
+    }
+    tauri_build::build()
 }
